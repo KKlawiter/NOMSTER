0f1f540ab99de30a3e48cb8ea31601cf017c4bfc
KKlawiter <katrinaklawiter@gmail.com>
2019-12-25 18:26:42 +0000

    Lesson 39

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,11 +1,12 @@
 class Place < ApplicationRecord
-	belongs_to :user
-	has_many :comments
-	
-	geocoded_by :address
-  	after_validation :geocode
+  belongs_to :user
+  has_many :comments
+  has_many :photos
 
-	validates :name, presence: true, length: { minimum: 3 }
-  	validates :address, presence: true
-  	validates :description, presence: true
+  geocoded_by :address
+  after_validation :geocode 
+  
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :address, presence: true
+  validates :description, presence: true
 end
